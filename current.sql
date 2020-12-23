e61150f85af9111de8028ed25dbaf047da3160e9
Dong1414 <homar1@naver.com>
2020-12-23 12:06:18 +0900

    오픈그래프1

--- current.sql
+++ current.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Community v13.1.5  (64 bit)
-MySQL - 10.4.6-MariaDB : Database - textBoard
+MySQL - 10.4.17-MariaDB : Database - textBoard
 *********************************************************************
 */
 
@@ -12,7 +12,7 @@ MySQL - 10.4.6-MariaDB : Database - textBoard
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`textBoard` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`textBoard` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
 USE `textBoard`;
 
@@ -24,17 +24,23 @@ CREATE TABLE `article` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `regDate` datetime NOT NULL,
   `updateDate` datetime NOT NULL,
-  `title` char(200) COLLATE utf8_unicode_ci NOT NULL,
-  `body` text COLLATE utf8_unicode_ci NOT NULL,
+  `title` char(200) NOT NULL,
+  `body` text NOT NULL,
   `memberId` int(10) unsigned NOT NULL,
   `boardId` int(10) unsigned NOT NULL,
+  `hit` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `article` */
 
-insert  into `article`(`id`,`regDate`,`updateDate`,`title`,`body`,`memberId`,`boardId`) values 
-(1,'2020-12-17 13:20:31','2020-12-17 13:20:31','제목_0.23634880151259602','# 공지사항\r\n 안녕하세요.\r\n이 사이트는 저의 글 연재 공간입니다.\r\n\r\n---\r\n\r\n# 이 사이트의 특징\r\n- A\r\n- B\r\n- C',2,2);
+insert  into `article`(`id`,`regDate`,`updateDate`,`title`,`body`,`memberId`,`boardId`,`hit`) values 
+(1,'2020-12-18 09:02:49','2020-12-18 09:02:49','공지사항?','# 공지사항\r\n안녕하세요.\r\n이 사이트는 저의 글 연재 공간입니다.\r\n\r\n---\r\n\r\n# 이 사이트의 특징\r\n- A\r\n- B\r\n- C',2,1,3),
+(2,'2020-12-18 09:02:49','2020-12-18 09:02:49','[Java] 기본 문법?','# 자바기본문법\r\n```java\r\nint a = 10;\r\nint b = 20;\r\nint c = a + b;\r\n```',2,2,2),
+(3,'2020-12-18 09:02:49','2020-12-18 09:02:49','[Java] 조건문?','# 조건문 ( if ) ?\r\n```java\r\nint a = 10;\r\n\r\nif(a == 10){ //a가 10이면 출력\r\n    System.out.println(a); \r\n}\r\n```',2,2,2),
+(4,'2020-12-18 11:07:44','2020-12-18 11:07:39','[Java] 두 숫자 사이의 합 구하기?','# 정수 a, b 가 주어질 경우 두 숫자 사이의 합 구하기 ?\r\n```java\r\nMath.min(a,b) //최소값\r\nMath.max(a,b) //최대값\r\n//등차수열\r\nprivate long sumAtob(long a((최대), long b(최소)){ return (b -1 a+ 1) * ( a + b ) / 2; } // \r\n}\r\n```',2,2,1),
+(5,'2020-12-18 11:10:40','2020-12-18 11:10:43','[Java] 진수 변환?','# 2진수, 8진수, 16진수,10진수 변환 ?\r\n```java\r\nimport java.util.Scanner;\r\n\r\npublic class Main {\r\n\r\n	public static void main(String[] args) {\r\n		\r\n		Scanner scan = new Scanner(System.in);\r\n		int num = scan.nextInt();\r\n		\r\n		String n2 = Integer.toBinaryString(num); // 2진수\r\n		String n8 = Integer.toOctalString(num); // 8진수\r\n		String n16 = Integer.toHexString(num); // 16진수\r\n		\r\n		\r\n		System.out.println(\"2진수: \" + n2);\r\n		System.out.println(\"8진수: \" + n8);\r\n		System.out.println(\"16진수: \" + n16);\r\n		\r\n        \r\n        String num8 = scan.nextLine(); // 8진수 입력받기\r\n     	int n10 = Integer.parseInt(num, 8);   //8진수 10진수로 변환, 입력에 따라 2,8,16\r\n        System.out.println(n10);\r\n	}\r\n}}\r\n```',2,2,3),
+(6,'2020-12-18 11:13:05','2020-12-18 11:13:07','[Java] 공백 제거?','# .trim() 공백제거 ( Scanner 메서드) ?\r\n```java \r\nScanner scan = new Scanner(System.in);\r\n\r\nString title = scan.nextLine().trim();\r\n\r\n```',2,2,1);
 
 /*Table structure for table `board` */
 
@@ -44,16 +50,16 @@ CREATE TABLE `board` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `regDate` datetime NOT NULL,
   `updateDate` datetime NOT NULL,
-  `name` char(20) COLLATE utf8_unicode_ci NOT NULL,
-  `code` char(20) COLLATE utf8_unicode_ci NOT NULL,
+  `name` char(20) NOT NULL,
+  `code` char(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `board` */
 
 insert  into `board`(`id`,`regDate`,`updateDate`,`name`,`code`) values 
-(1,'2020-12-17 13:20:31','2020-12-17 13:20:31','공지사항','notice'),
-(2,'2020-12-17 13:20:31','2020-12-17 13:20:31','자유','free');
+(1,'2020-12-18 09:02:49','2020-12-18 09:02:49','공지사항','notice'),
+(2,'2020-12-18 09:02:49','2020-12-18 09:02:49','IT','it');
 
 /*Table structure for table `member` */
 
@@ -63,17 +69,17 @@ CREATE TABLE `member` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `regDate` datetime NOT NULL,
   `updateDate` datetime NOT NULL,
-  `loginId` char(30) COLLATE utf8_unicode_ci NOT NULL,
-  `loginPw` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `name` char(30) COLLATE utf8_unicode_ci NOT NULL,
+  `loginId` char(30) NOT NULL,
+  `loginPw` varchar(50) NOT NULL,
+  `name` char(30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `member` */
 
 insert  into `member`(`id`,`regDate`,`updateDate`,`loginId`,`loginPw`,`name`) values 
-(1,'2020-12-17 13:20:31','2020-12-17 13:20:31','test1','test1','테스터1'),
-(2,'2020-12-17 13:20:31','2020-12-17 13:20:31','test2','test2','테스터2');
+(1,'2020-12-18 09:02:49','2020-12-18 09:02:49','test1','test1','테스터1'),
+(2,'2020-12-18 09:02:49','2020-12-18 09:02:49','1','1','호말');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
